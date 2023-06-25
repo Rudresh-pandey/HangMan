1481ee33714f973df87ec0868f5aa8298adca1f5
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-06-25 12:38:16 +0530

    GuessWord added

--- src/GuessWord.tsx
+++ src/GuessWord.tsx
@@ -1,7 +1,24 @@
-function GuessWord() {
+type HangmanWordProps = {
+  guessedLetters : string[],
+  wortToGuess: string
+  reveal?: boolean
+}
+
+function GuessWord({guessedLetters, wordToGuess , reveal=false}: HangmanWordProps) {
+  
   return (
-    <div>
-      
+    <div style={{ display: "flex", gap:".25em", fontSize:"4rem", fontWeight: "bold", fontFamily:"monospace", textTransform:"uppercase"}}>
+      {wordToGuess.split("").map((letter, index)=>{
+        return(
+          <span style={{borderBottom: ".1em solid black"}} key={index} >
+            <span style={{
+              visibility:guessedLetters.includes(letter) || reveal ?"visible":"hidden" ,
+              color: !guessedLetters.includes(letter) && reveal ? "red" : "black",
+            }}>
+            {letter}</span>
+            </span>
+        )
+      })}
     </div>
   )
 }
