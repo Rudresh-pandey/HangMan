f8e77937b7e222dda7c67f46e3f02e00e19ac4a7
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-06-25 12:39:12 +0530

    keyboard and css added

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,8 +1,30 @@
+import styles from './KeyBoard.module.css'
 
-function Keyboard() {
+const KEYS = [
+  "a",
+  "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+  "x","y","z",
+]
+
+type keyboardProps = {
+  activeLetters: string[] 
+  inactiveLetters: string[] 
+  addGuessedLetter:(letter:string)=>void
+  disabled?: boolean
+}
+
+function Keyboard({activeLetters, inactiveLetters, addGuessedLetter, disabled= false}:keyboardProps) {
   return (
-    <div>
-      
+    <div style={{display:"grid", gridTemplateColumns:"repeat(auto-fit,minmax(75px, 1fr))", gap:".5rem"}}>
+      {KEYS.map(key=>{
+        const isActive = activeLetters.includes(key)
+        const isInActive = inactiveLetters.includes(key)
+        return(
+          <button onClick={()=>addGuessedLetter(key)} className={ `${styles.btn} ${isActive ? styles.active: ""} ${isInActive? styles.inactive:""} `}
+          disabled= {isInActive || isActive || disabled}
+          key={key}>{key}</button>
+        )
+      })}
     </div>
   )
 }
